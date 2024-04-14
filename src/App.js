4b45ddf9d6650d1787838585c35be31692420e91
Disinia <disiniakunjumon@gmail.com>
2024-04-14 17:39:48 +0530

    Saerch Recipie Completed

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
 import Addrecipie from './components/Addrecipie';
+import Searchrecipie from './components/Searchrecipie';
 
 function App() {
   return (
     <div>
 
       <Addrecipie />
+
+      <Searchrecipie/>
     </div>
   );
 }
